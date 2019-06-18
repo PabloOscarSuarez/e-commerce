a0caccd533c1d1f06328794d946289f4efe4b138
PabloOscarSuarez <pablooscarsuarez@gmail.com>
2019-06-18 15:17:11 -0300

    configuracion terminada back-front

---
+++ front/src/components/MainContainer/Main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+// import { fetchLoggedUser } from '../redux/actions/user';
+
+// import NavbarContainer from '../containers/NavbarContainer';
+
+export default () => {
+  return <div>hola ya pude entrar</div>;
+};
--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,16 @@
+// el inicio de la app
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/app/App";
-import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Main from "./components/MainContainer/Main";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Route path="/" component={Main} />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("app")
+);
