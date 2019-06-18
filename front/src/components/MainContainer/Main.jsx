ade4476f31deb147c971453f85279eb70addd2a5
PabloOscarSuarez <pablooscarsuarez@gmail.com>
2019-06-18 18:11:11 -0300

    editando pages

--- front/src/components/MainContainer/Main.jsx
+++ front/src/components/MainContainer/Main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Route, Redirect, Switch } from "react-router-dom";
+//terminar navbar soy pablo
+// import { Route, Redirect, Switch } from "react-router-dom";
 // import { fetchLoggedUser } from '../redux/actions/user';
 
 // import NavbarContainer from '../containers/NavbarContainer';
 
 export default () => {
-  return <div>hola ya pude entrar</div>;
+  return <div />;
 };
