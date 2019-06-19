6b7204fe432c828991557d31688edabc567b5a93
alejandrogvillarreal <alejandrogvillarreal@gmail.com>
2019-06-19 11:50:04 -0300

    models user, book, author, genre ok

---
+++ back/db/models/book.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const db = require('../../db');
+
+var Book = db.define('book', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    stock: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate :{
+            min: 0
+        }
+    },
+});
+
+module.exports = Book
--- front/src/components/MainContainer/Main.jsx
+++ front/src/components/MainContainer/Main.jsx
@@ -1,17 +1,11 @@
 import React from "react";
-<<<<<<< HEAD
 import { Route, Switch } from "react-router-dom";
 import Book from "../ClientsComponents/BookContainer/Book";
 import BookComponent from "../ClientsComponents/BookContainer/Book";
-=======
-//terminar navbar soy pablo
-// import { Route, Redirect, Switch } from "react-router-dom";
->>>>>>> ade4476f31deb147c971453f85279eb70addd2a5
 // import { fetchLoggedUser } from '../redux/actions/user';
 // import NavbarContainer from '../containers/NavbarContainer';
 
 export default () => {
-<<<<<<< HEAD
   return (
     <div>
       <Switch>
@@ -19,7 +13,4 @@ export default () => {
       </Switch>
     </div>
   );
-=======
-  return <div />;
->>>>>>> ade4476f31deb147c971453f85279eb70addd2a5
 };
