2223dd4b93c3d38a51dca89b23c74089fbccab93
PabloOscarSuarez <pablooscarsuarez@gmail.com>
2019-06-18 21:09:45 -0300

    probando el merge

--- front/src/index.js
+++ front/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Main from "./components/MainContainer/Main";
-
+///comentemos todo lo que quieran quiero ver si funciona el merge jajaja
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
